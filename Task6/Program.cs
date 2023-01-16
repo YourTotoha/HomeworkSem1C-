cf46678687467223b74223d57d70d15bf9d18015
Vladimir <saintprize@gmail.com>
2023-01-16 06:48:49 +0300

    Добавил решенную задачу 6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа для проверки, делится ли число на 2 без остатка
+Console.Write("Введитечисло: ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
