6eeed7a02aaa2c27d6e71e09f422fe9c747e3435
Vladimir <saintprize@gmail.com>
2023-01-16 06:10:45 +0300

    Добавил решение ко 2 задаче

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа для проверки какое число больше
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b )
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+     Console.WriteLine("max = " + b);
+}
